5f49ecda3515061eee3e54b0b1826244e42bed17
alexandre-Maury <alexandre_maury@outlook.com>
2024-09-26 14:19:40 +0200

    maj

--- pre-install.sh
+++ pre-install.sh
@@ -6,19 +6,31 @@ set -euo pipefail
 # Définir l'IFS (Internal Field Separator) pour gérer correctement les espaces blancs
 IFS=$'\n\t'
 
-# Fonction pour afficher les messages de statut
-status_message() {
-    echo "[*] $1"
+# Fonction pour afficher les messages de statut jaune
+status_message_system() {
+    echo "\033[0;33m [*] $1 \033[0m"
 }  
 
+# Fonction pour afficher les messages d'erreur rouge
+status_message_error() {
+    echo "\033[0;31m [*] $1 \033[0m"
+}
+
+# Fonction pour afficher les messages de succés vert
+status_message_succes() {
+    echo "\033[0;32m [*] $1 \033[0m"
+}
+
+
 # Obtenir l'utilisateur courant
-read -p "[*] Veuillez entrer votre nom d'utilisateur pour la suite du script : " USER_TO_ADD
+echo -e "[*] Veuillez entrer votre nom d'utilisateur pour la suite du script : " 
+read USER_TO_ADD
 
 
 # Vérifier si l'utilisateur est déjà dans le groupe sudo
 if groups "$USER_TO_ADD" | grep -q "\bsudo\b"; then
     clear
-    echo "$USER_TO_ADD est déjà dans le groupe sudo."
+    status_message_system "$USER_TO_ADD est déjà dans le groupe sudo."
 
 else
     echo "$USER_TO_ADD n'est pas dans le groupe sudo. Ajout en cours..."
@@ -27,8 +39,9 @@ else
     usermod -aG sudo $USER_TO_ADD
 
     clear
-    printf "%s \\n" "[Succès] Votre compte : ${USER_TO_ADD} est à présent membre du groupe sudo"
+    status_message_succes "Votre compte : ${USER_TO_ADD} est à présent membre du groupe sudo"
 fi
 
 
-status_message "Run : bash -x install.sh -> Ce script ne doit pas être exécuté en tant qu'utilisateur root."
+status_message_system "Run : bash -x install.sh"
+status_message_error "Ce script ne doit pas être exécuté en tant qu'utilisateur root."
